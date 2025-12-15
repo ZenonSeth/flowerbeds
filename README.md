57ca348f7822a161aa3cdce31be26af481418c55
Zenon Seth <Zenon.Seth@gmail.com>
2025-12-15 12:22:22 +0000

    MCL/VL support. Update textures and model. Update spread settings

--- README.md
+++ README.md
@@ -1,9 +1,65 @@
 # Flowerbeds
-Flowerbeds are blocks that allow easier growing of flowers.
-Once you place a flower on top of a flowerbed, it will quickly grow in nearby empty flowerbeds.
+
+## Description
+
+Flowerbeds are blocks that allow easier growing of flowers. Once you place a flower on top of a flowerbed, it will grow in nearby empty flowerbeds.
 
 This growing is not limited by the usual restriction of flora density, so a single flower planted in a large flowerbed field will eventually fill the entire field with the flower.
 
-In order to grow new flowers, flowerbeds must be on the same vertical height, and adjecent - even diagonally - to each other. Flowerbeds also do not work at all at depths of -50 or deeper.
+In order to grow new flowers, flowerbeds must be on the same vertical height, and adjacent - even diagonally - to each other.
 
-Flowerbeds are crafted from 1 coal, 1 dirt and 1 wood placed vertically. Different woods yields different looking flowerbeds, but they all share the same functionality and can work with each other.
+Flowerbeds are crafted from 1 coal, 1 dirt and 1 wood placed vertically.
+Different woods yields different looking flowerbeds, but they all share the same functionality and can work with each other.
+
+Flower beds have a random time between checks, and random probability of success, to see if flowers grow in empty flowerbeds. The times and probability can be controlled with the [Settings] listed below.
+
+## Flower Compatibility
+
+Flowerbeds grow flowers based on groups, so any mod that adds new flowers and correctly tags them with the "flower" group should be compatible with Flowerbeds.
+
+### Game Compatibility
+
+Depending on the game you are playing the following mods should be present:
+
+* Minetest Game, and all derivatives: `default`. Specifically needs wood textures to be present and sounds functions to be present.
+* Mineclonia: `mcl_core`, `mcl_trees`, `mcl_sounds`
+* Voxelibre: `mcl_core`, `mcl_bamboo`, `mcl_mangrove`, `mcl_cherry_blossom`, `mcl_sounds`
+
+## Settings
+
+### flowerbeds_min_spread_time
+
+This setting sets the minimum time between checks.
+
+Defaults to 25 seconds.
+
+### flowerbeds_max_spread_time
+
+This setting sets the maximum time between checks.
+
+Defaults to 45 seconds.
+
+### flowerbeds_min_removed_time
+
+Once a flower bed has a flower it will regularly check to see if the flower has
+been removed and if it should check to see if a flower spreads to it. This is
+the minimum time for those checks.
+
+Defaults to 80 seconds.
+
+### flowerbeds_max_removed_time
+
+This is the maximum time for checks to see if a flower has been removed.
+
+Defaults to 120 seconds.
+
+### flowerbeds_chance
+
+This is the chance out of 100 for an adjacent flower to spread to this flower
+bed.
+
+Defaults to 25.
+
+## Credits
+- Main development: [Zenon Seth](https://github.com/ZenonSeth)
+- Additional work: [Codiac](https://codeberg.org/Codiac)
--- init.lua
+++ init.lua
@@ -1,80 +1,251 @@
--- flowerbeds = {}
--- local flowerbedNodes = {}
-local baseMatsAndTextures = {
-	{"default:wood", "default_wood.png"},
-	{"default:junglewood", "default_junglewood.png"},
-	{"default:acacia_wood", "default_acacia_wood.png"},
-	{"default:pine_wood", "default_pine_wood.png"},
-	{"default:aspen_wood", "default_aspen_wood.png"}
-	-- perhaps too many, unecessary
-	-- {"default:stonebrick", "default_stone_brick.png"},
-	-- {"default:stone_block", "default_stone_block.png"},
-	-- {"default:desert_stonebrick", "default_desert_stone_brick.png"},
-	-- {"default:desert_stone_block", "default_desert_stone_block.png"},
-	-- {"default:sandstonebrick", "default_sandstone_brick.png"},
-	-- {"default:sandstone_block", "default_sandstone_block.png"},
-	-- {"default:desert_sandstone_brick", "default_desert_sandstone_brick.png"},
-	-- {"default:desert_sandstone_block", "default_desert_sandstone_block.png"},
-	-- {"default:silver_sandstone_brick", "default_silver_sandstone_brick.png"},
-	-- {"default:silver_sandstone_block", "default_silver_sandstone_block.png"}
-}
+local pRandom = PcgRandom(os.time() + 42) -- H2G2
 
-local getBasicFlowerbedDef = function(baseTexture) 
-	return {
-		drawtype = "normal",--"glasslike_framed",
-		description = "Basic Flowerbed",
-		tiles = {
-				baseTexture.."^flowerbed_basic.png",
-				baseTexture.."^flowerbed_trim.png"
-		},
-		connects_to = {"group:basicFlowerbed"},
-		groups = {choppy = 3, oddly_breakable_by_hand = 3, flowerbed = 1},
-		sounds = default.node_sound_wood_defaults()
-	}
-end
-
-local basic_flowerbed_abm = function(pos, node, active_object_count, active_object_count_wider)
-	local checkAdj = {{1, 1}, {1, 0}, {1, -1}, {0, 1}, {0, -1}, {-1, 1}, {-1, 0}, {-1, -1}}
-	pos.y = pos.y + 1
-	local nodeAbove = minetest.get_node_or_nil(pos)
-	if not nodeAbove or nodeAbove.name ~= "air" then
-		return
-	end
-	for _, adj in pairs(checkAdj) do
-		local checkPos = {x = pos.x + adj[1], y = pos.y, z = pos.z + adj[2]}
-		local checkNode = minetest.get_node_or_nil(checkPos)
-		if checkNode and minetest.get_item_group(checkNode.name, "flower") > 0 then
-			minetest.set_node(pos, checkNode) -- copy the flower
-			return
-		end
-	end
-end
 --------------------------------
--- minetest calls
+-- Fake player, needed since we call nodes' on_place function, if existing
+--------------------------------
+local function create_fake_player()
+  local fakePlayer = {
+    get_player_name = function() return "" end,
+    is_player = function() return true end,
+    is_fake_player = true,
+    get_player_control = function() return {
+        jump = false, right = false, left = false, LMB = false, RMB = false,
+        sneak = false, aux1 = false, down = false, up = false
+      } end,
+    get_player_control_bits = function() return 0 end,
+  }
+  return fakePlayer
+end
+
+local minimalFakePlayer = create_fake_player()
+
+--------------------------------
+-- General setup
+--------------------------------
+
+local function clamp(v, min, max)
+  v = tonumber(v) or min
+  if v < min then return min
+  elseif v > max then return max
+  else return v end
+end
+
+local minSpreadTime = clamp(core.settings:get("flowerbeds_minSpreadTime") or 25, 5, 600)
+local maxSpreadTime = clamp(core.settings:get("flowerbeds_maxSpreadTime") or 45, minSpreadTime, 600)
+local minRemovedTime = clamp(core.settings:get("flowerbeds_minRemovedTime") or 80, 5, 600)
+local maxRemovedTime = clamp(core.settings:get("flowerbeds_maxRemovedTime") or 120, minRemovedTime, 600)
+local spreadChance = clamp(core.settings:get("flowerbeds_chance") or 25, 1, 100)
+
+local blockGroups = { flowerbed = 1 }
+local addMclHardness = false
+local seeds = {}
+
+local baseMatsAndTextures = {}
+local sounds = {}
+local materials = {}
+
+-- compat with default and mcl
+if default then
+  baseMatsAndTextures = {
+    {"default:wood", "default_wood.png"},
+    {"default:junglewood", "default_junglewood.png"},
+    {"default:acacia_wood", "default_acacia_wood.png"},
+    {"default:pine_wood", "default_pine_wood.png"},
+    {"default:aspen_wood", "default_aspen_wood.png"},
+  }
+	blockGroups.choppy = 3
+	blockGroups.oddly_breakable_by_hand = 3
+	sounds = default.node_sound_wood_defaults()
+  sounds.footstep = default.node_sound_dirt_defaults().footstep
+  materials.coal_lump = "default:coal_lump"
+  materials.dirt = "default:dirt"
+elseif mcl_trees and mcl_trees.woods and mcl_sounds then -- only mineclonia has mcl_trees.woods
+	addMclHardness = true
+	sounds = mcl_sounds.node_sound_wood_defaults()
+  sounds.footstep = mcl_sounds.node_sound_dirt_defaults().footstep
+  materials.coal_lump = "mcl_core:coal_lump"
+  materials.dirt = "mcl_core:dirt"
+  seeds = {
+    "mcl_farming:wheat_seeds",
+    "mcl_farming:potato_item",
+    "mcl_farming:carrot_item",
+    "mcl_farming:beetroot_seeds",
+  }
+
+  local wood_types = {"bark", "wood", "stripped"}
+  blockGroups.grass_block = 1
+  blockGroups.soil = 1
+  blockGroups.soil_sapling = 2
+  blockGroups.soil_sugarcane = 1
+  blockGroups.soil_bamboo = 1
+  blockGroups.soil_fungus = 1
+  blockGroups.supports_mushrooms = 1
+	blockGroups.handy = 1
+	blockGroups.axey = 1
+
+  for name, _ in pairs(mcl_trees.woods) do
+    for _, wood_type in ipairs(wood_types) do
+      local nodeName = "mcl_trees:" .. wood_type .. "_" .. name
+      local nodeDef = core.registered_nodes[nodeName]
+      if nodeDef then
+        local texture = nodeDef.tiles[1]
+        if #nodeDef.tiles >= 3 then texture = nodeDef.tiles[3] end
+        table.insert(baseMatsAndTextures, {nodeName, texture})
+      end
+    end
+  end
+elseif mcl_core then -- voxelibre support, couldn't find wood table, hardcode some woods
+	addMclHardness = true
+	sounds = mcl_sounds.node_sound_wood_defaults()
+  sounds.footstep = mcl_sounds.node_sound_dirt_defaults().footstep
+  materials.coal_lump = "mcl_core:coal_lump"
+  materials.dirt = "mcl_core:dirt"
+  blockGroups.grass_block = 1
+	blockGroups.handy = 1
+	blockGroups.axey = 1
+
+  local plankNodes = {
+    "mcl_bamboo:bamboo_plank", "mcl_cherry_blossom:cherrywood", "mcl_core:acaciawood", "mcl_core:birchwood",
+    "mcl_core:darkwood", "mcl_core:junglewood", "mcl_core:sprucewood", "mcl_core:wood","mcl_mangrove:mangrove_wood",
+  }
+  for _, nodeName in ipairs(plankNodes) do
+    local nodeDef = core.registered_nodes[nodeName]
+    if nodeDef then
+      local texture = nodeDef.tiles[1]
+      table.insert(baseMatsAndTextures, {nodeName, texture})
+    end
+  end
+end
+
+local function start_flowerbed_timer(pos, min, max)
+  local timer = core.get_node_timer(pos)
+  timer:stop()
+  timer:start(pRandom:next(min, max))
+end
+
+local function on_flowerbed_timer(pos, _)
+  local flower_pos = vector.offset(pos, 0, 1, 0)
+  local nodeAbove = core.get_node_or_nil(flower_pos)
+
+  if nodeAbove and nodeAbove.name == "air" then
+    local p1 = vector.offset(flower_pos, 1, 0, 1)
+    local p2 = vector.offset(flower_pos, -1, 0, -1)
+    local flowers = core.find_nodes_in_area(p1, p2, {"group:flower", "group:flora"})
+    if flowers and #flowers > 0 then
+      if pRandom:next(1, 100) <= spreadChance then
+        local chosen = flowers[pRandom:next(1, #flowers)]
+        if false and table.indexof(seeds, chosen) ~= -1 then chosen = seeds[chosen] end
+
+        local node = core.get_node(chosen)
+        local def = core.registered_nodes[node.name]
+        if def.drop and type(def.drop) == "string" and def.drop ~= '' then
+          if string.find(def.drop, ' ') then
+            node.name = string.sub(def.drop, 1, string.find(def.drop, ' ') - 1)
+          else
+            node.name = def.drop
+          end
+        end
+
+        if node then
+          local itemDef = core.registered_items[node.name]
+          if itemDef and type(itemDef.on_place) == "function" then
+            itemDef.on_place(ItemStack(node.name), minimalFakePlayer, {type = "node", above = flower_pos, under = pos})
+          else
+            core.set_node(flower_pos, node)
+          end
+        end
+      end
+    end
+    start_flowerbed_timer(pos, minSpreadTime, maxSpreadTime)
+    return false
+  else -- node above was air, or nil
+    if nodeAbove then
+      local wild = core.get_item_group(nodeAbove.name, "wildflower")
+
+      if wild > 0 and wild < 4 then
+        local node_name, num = nodeAbove.name:match("(.-)(%d*)$")
+        num = tonumber(num) or 0
+        local new_name = node_name .. (num + 1)
+        core.swap_node(flower_pos, {name = new_name})
+        start_flowerbed_timer(pos, minRemovedTime * 3, maxRemovedTime * 6)
+        return false
+      end
+    end
+
+    -- If we are covered then run timer slower to see if it's been removed
+    start_flowerbed_timer(pos, minRemovedTime, maxRemovedTime)
+    return false
+  end
+end
+
+local flowebedLipHeight = 9/16
+local getBasicFlowerbedDef = function(baseTexture)
+  local ret = {
+    drawtype = "nodebox",
+    description = "Flowerbed",
+    tiles = {baseTexture .. "^flowerbed_basic.png", baseTexture .. "^flowerbed_trim.png"},
+    connects_to = {"group:flowerbed"},
+    node_box = {
+      type = "connected",
+      fixed = {
+        -1/2, -1/2, -1/2, 1/2, 1/2, 1/2
+      },
+      disconnected_left = {
+        -8/16, -8/16, -8/16, -7/16, flowebedLipHeight, 8/16
+      },
+      disconnected_right = {
+        8/16, -8/16, 8/16, 7/16, flowebedLipHeight, -8/16
+      },
+      disconnected_front = {
+        -8/16, -8/16, -8/16, 8/16, flowebedLipHeight, -7/16
+      },
+      disconnected_back = {
+       8/16, -8/16, 8/16, -8/16, flowebedLipHeight, 7/16
+      },
+    },
+    groups = blockGroups,
+    sounds = sounds,
+    on_timer = on_flowerbed_timer,
+    on_construct = function(pos) start_flowerbed_timer(pos, minSpreadTime, maxSpreadTime) end,
+  }
+	if addMclHardness then
+		ret._mcl_blast_resistance = 0.8
+		ret._mcl_hardness = 0.6
+	end
+	return ret
+end
+
+--------------------------------
+-- Luanti calls
 --------------------------------
 
 for _, data in pairs(baseMatsAndTextures) do
-	local def = getBasicFlowerbedDef(data[2])
-	local name = "flowerbeds:basic_"..string.sub(data[1],9,-1)
-	minetest.register_node(name , def)
+  local nodeName = data[1]
+  if core.registered_nodes[nodeName] ~= nil then
+    local texture = data[2]
+    local def = getBasicFlowerbedDef(texture)
+    local name = "flowerbeds:basic_" .. string.sub(nodeName, string.find(nodeName, ':') + 1, -1)
 
-	minetest.register_craft({
-		output = name,
-		recipe = {
-			{"default:coal_lump"},
-			{"default:dirt"},
-			{data[1]}
-		}
-	})
+    core.register_node(name, def)
+
+    core.register_craft({
+      output = name,
+      recipe = {
+        {materials.coal_lump},
+        {materials.dirt},
+        {data[1]},
+      }
+    })
+  end
 end
 
-minetest.register_abm({
-	label = "ABM for Flowerbeds",
-	nodenames = {"group:flowerbed"},
-	neighbors = {"air"},
-	interval = 11.0,
-	chance = 40,
-	min_y = -50,
-	catch_up = true,
-	action = basic_flowerbed_abm
+core.register_lbm({
+  label = "Enable node timers on existing flower beds",
+  name = "flowerbeds:start_flowerbed_timer",
+  nodenames = {"group:flowerbed"},
+  run_at_every_load = false,
+  action = function(pos)
+    local timer = core.get_node_timer(pos)
+    if not timer:is_started() then start_flowerbed_timer(pos, minSpreadTime, maxSpreadTime) end
+  end,
 })
